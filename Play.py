b9be5aa2d0a02bd3dbf509cf05d7ec71c5274173
Rafi Halilintar <masitingss@gmail.com>
2018-12-20 04:20:25 +0700

    First Commit

--- Play.py
+++ Play.py
@@ -22,7 +22,7 @@ class device_handler(debounce_handler):
     """
     #TRIGGERS = {str(sys.argv[1]): int(sys.argv[2])}
     #TRIGGERS = {"office": 52000}
-    TRIGGERS = {"socket 1": 52000,"socket 2":51000}
+    TRIGGERS = {"socket 1": 53000,"socket 2":54000}
 
     def act(self, client_address, state, name):
         print("State", state, "from client @", client_address)
