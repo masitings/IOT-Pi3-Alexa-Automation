4c33278c5494304b4c0284fcd7ac456151ffc6bd
Rafi Halilintar <masitingss@gmail.com>
2018-12-20 04:15:43 +0700

    First Commit

--- RPi_name_port_gpio.py
+++ RPi_name_port_gpio.py
@@ -22,21 +22,21 @@ class device_handler(debounce_handler):
     """
     #TRIGGERS = {str(sys.argv[1]): int(sys.argv[2])}
     #TRIGGERS = {"office": 52000}
-    TRIGGERS = {"kitchen": 52000,"living room":51000}
+    TRIGGERS = {"socket 1": 52000,"socket 2":51000}
 
     def act(self, client_address, state, name):
         print("State", state, "from client @", client_address)
         # GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
         # GPIO.setup(int(7), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
         # GPIO.output(int(7), state) ## State is true/false
-        if name=="kitchen":
+        if name=="socket 1":
             GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-            GPIO.setup(int(7), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
-            GPIO.output(int(7), state) ## State is true/false
-        elif name =="living room":
+            GPIO.setup(int(35), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
+            GPIO.output(int(35), state) ## State is true/false
+        elif name =="socket 2":
             GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-            GPIO.setup(int(11), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
-            GPIO.output(int(11), state) ## State is true/false
+            GPIO.setup(int(37), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
+            GPIO.output(int(37), state) ## State is true/false
         else:
             print("Device not found!")
 
